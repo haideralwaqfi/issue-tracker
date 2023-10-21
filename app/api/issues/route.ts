1e2cb6bdcf56a65fd5d41ed042dfe514da806f1d
haideralwaqfi <haideralwaqfi@gmail.com>
2023-10-21 13:09:23 +0300

    creating issue API

---
+++ app/api/issues/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import { Prisma } from "@prisma/client";
+import prisma from "@/prisma/client";
+
+export async function POST(request: NextRequest) {
+  const createIssueSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1),
+  });
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
+}
