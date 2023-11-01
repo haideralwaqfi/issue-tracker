d5afbd6136ac63ff8b1e1a47a778c618cb8963bb
haideralwaqfi <haideralwaqfi@gmail.com>
2023-11-01 22:15:45 +0300

    build the layout of pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Pagination currentPage={2} pageSize={10} itemCount={11} />
+    </main>
   );
 }
